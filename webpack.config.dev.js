c15bd1c89093a1d3da69a00d76c75a52c0954139
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-08-02 22:17:29 +0200

    setting up dev enviroment for node

---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'main.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), use: ['babel-loader']}
+    ]
+  }
+};
