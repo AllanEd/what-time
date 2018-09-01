f42f37ae53ec8069e05ab642619f4a8abacab995
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-09-01 21:13:29 +0200

    removed react router and implemented react router dom

--- common/routes.jsx
+++ common/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
-import history from './history';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createBrowserHistory, createMemoryHistory } from 'history';
 
 import reduxStore from './store/reduxStore';
 
@@ -9,6 +9,8 @@ import AppointmentsPage from './containers/AppointmentsPage';
 
 import './scss/main.scss';
 import 'normalize.css';
+
+const history = typeof window !== 'undefined' ? createBrowserHistory() : createMemoryHistory();
 
 const { login } = reduxStore.getState();
 const isUserLoggedIn = login !== null;
